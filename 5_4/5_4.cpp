ad9b15b2440fc58e767344c87a47e17c79106fd1
lenovo <lenovo@LAPTOP-P8K9KTT5>
2020-03-27 14:36:50 +0800

    5_4

--- 5_4/5_4.cpp
+++ 5_4/5_4.cpp
@@ -2,10 +2,43 @@
 //
 
 #include "stdafx.h"
-
+#include <opencv.hpp>
+using namespace cv;
 
 int main()
 {
-    return 0;
-}
+	VideoCapture cap;
 
+	cap.open(0);
+
+	if (!cap.isOpened())
+	{
+		std::cout << "不能打开视频文件" << std::endl;
+		return-1;
+	}
+
+	double fps = cap.get(CAP_PROP_FPS);
+	std::cout << "fps" << fps << std::endl;
+	while (1)
+	{
+		cv::Mat frame;
+		cv::Mat dx;
+		cv::Mat dy;
+		bool rSucess = cap.read(frame);
+		if (!rSucess)
+		{
+			std::cout << "不能从视频文件中读取帧" << std::endl;
+			break;
+		}
+		else
+		{
+			Sobel(frame, dx, CV_16SC1, 1, 0, 5);
+			Sobel(frame, dy, CV_16SC1, 0, 1, 5);
+			cv::imshow("原图", frame);
+			cv::imshow("x方向上的边缘提取", dx);
+			cv::imshow("y方向上的边缘提取", dy);
+		}
+		waitKey(30);
+	}
+	return 0;
+}
